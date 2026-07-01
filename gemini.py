9c1b8e22c6078f0d6d2df2cc44e175dae32adc2c
Reyansh Mukhija <reyanshmukhija2014@gmail.com>
2026-07-01 19:23:34 +0530

    Delete gemini.py

--- gemini.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AQ.Ab8RN6K6VoDLcZPRLCq8fOkU7GAyLo2GGIgS6ICV5WqrkEY-kw")  
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Tell me a fun fact about RC fighter jets"
-)
-print(response.text)
